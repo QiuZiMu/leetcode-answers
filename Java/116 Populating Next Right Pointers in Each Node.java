98abd8fbd108d099e2ad24e26ea198360b1d643c
QiuZiMu <yuemengsen@gmail.com>
2022-08-14 12:09:02 +0800

    20220814

---
+++ Java/116 Populating Next Right Pointers in Each Node.java
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    //递归树 每棵树的右叶子节点的next为父节点的next的左叶子节点
+    public Node connect(Node root) {
+         if (root == null) {
+            return root;
+        }
+
+        if (root.left != null) {
+            root.left.next = root.right;
+            root.right.next = root.next == null ? null : root.next.left;
+            connect(root.left);
+            connect(root.right);
+        }
+        return root;
+    }
+}
